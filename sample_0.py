287e4095d287d53f33ec2d6eaf929fab89192afe
suzu <suzu@example.com>
2021-11-08 16:48:10 +0900

    sample_2

--- sample_0.py
+++
@@ -1,7 +0,0 @@
-# import関連
-
-import numpy as np  # ベクトルや行列の計算、高校生以上で習うような計算処理
-import matplotlib.pyplot as plt  # 画像の表示
-import matplotlib.animation as animation  # アニメーションの表示
-import pandas as pd  # csvファイルの読み込み
-import itertools
